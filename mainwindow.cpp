39f16ab1d7293c60185bb5fc702775f94c70c124
danclas <danvershinin@gmail.com>
2021-11-08 22:42:23 +0300

    Setup y=x^2 plot

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,25 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    h = 0.1;
+    xBegin = -3;
+    xEnd = 3 + h;
+    ui->widget->xAxis->setRange(-4, 4);
+    ui->widget->yAxis->setRange(0, 9);
+
+    N = (xEnd - xBegin)/h + 2;
+
+    for(X = xBegin; X <= xEnd; X+=h)
+    {
+        x.push_back(X);
+        y.push_back(X*X);
+    }
+
+    ui->widget->addGraph();
+    ui->widget->graph(0)->addData(x,y);
+    ui->widget->replot();
+
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    double xEnd, xBegin, h, X;
+    int N;
+
+    QVector<double> x,y;
 };
 #endif // MAINWINDOW_H
