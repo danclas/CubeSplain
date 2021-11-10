8df1ca7315432ca1340fd26fee75fe57e70136d0
danclas <danvershinin@gmail.com>
2021-11-10 20:05:21 +0300

    Add input_Btn

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,3 +24,19 @@ void MainWindow::on_point_table_itemChanged(QTableWidgetItem *item)
         ui->point_table->setRowCount(row_count + 1);
     }
 }
+
+void MainWindow::on_input_Btn_clicked()
+{
+    QVector<double> x;
+    QVector<double> y;
+
+    for (int i = 0; i < ui->point_table->rowCount() - 1; i++ )
+    {
+        auto x_text = ui->point_table->item(i, 0)->text();
+        auto y_text = ui->point_table->item(i, 1)->text();
+
+        if(x_text != "") { x.push_back(x_text.toDouble()); }
+        if(y_text != "") { y.push_back(y_text.toDouble()); }
+    }
+
+}
