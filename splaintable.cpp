68051edc3e5ec1ec8c2d7762f83e4b58bc7add2d
danclas <danvershinin@gmail.com>
2021-11-24 12:24:10 +0300

    Add fill splain coeff table

--- splaintable.cpp
+++ splaintable.cpp
@@ -15,5 +15,17 @@ splainTable::~splainTable()
 
 void splainTable::getTable(systemOfCubicSplines system)
 {
-    ui->tableWidget->setRowCount(system.get_N());
+    ui->tableWidget->clearContents();
+    ui->tableWidget->setRowCount(1);
+    int N = system.get_N();
+    ui->tableWidget->setRowCount(N);
+    auto list = system.getCubicSplineList();
+
+    for (int i = 0; i < N; i++) {
+         ui->tableWidget->setItem(i, 0, new QTableWidgetItem(QString::number(list[i].get_a())));
+         ui->tableWidget->setItem(i, 1, new QTableWidgetItem(QString::number(list[i].get_b())));
+         ui->tableWidget->setItem(i, 2, new QTableWidgetItem(QString::number(list[i].get_c())));
+         ui->tableWidget->setItem(i, 3, new QTableWidgetItem(QString::number(list[i].get_d())));
+    }
+    ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers); //read-only items on table
 }
